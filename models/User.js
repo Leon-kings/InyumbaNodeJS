7003ceea85d450ed05e54a6a809be493c583dfc2
Leon-kings <leonakingeneye2002@gmail.com>
2026-08-13 19:32:11 +0200

    changes in user model

--- models/User.js
+++ models/User.js
@@ -47,7 +47,7 @@ const userSchema = new mongoose.Schema(
 
     role: {
       type: String,
-      enum: ["user", "admin", "host"],
+      enum: ["user", "admin", "host", "manager"],
       default: "user",
     },
 
